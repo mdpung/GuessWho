bd1a59cd84090a0a7371381f0d5ebbae77cc3c34
Mitchell Pung <mdpung@gmail.com>
2021-05-15 02:43:47 -0500

    Replaced template variables with wrong file name.

--- server.js
+++ server.js
@@ -6,11 +6,11 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static(__dirname + '/dist/GuessWho'));
+app.use(express.static(__dirname + '/dist/guess-who'));
 
 app.get('/*', function(req,res) {
     
-res.sendFile(path.join(__dirname+'/dist/GuessWho/index.html'));
+res.sendFile(path.join(__dirname+'/dist/guess-who/index.html'));
 });
 
 // Start the app by listening on the default Heroku port
